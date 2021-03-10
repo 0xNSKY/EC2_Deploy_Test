bdd135dde5cf3808c41a28dfe4f56bdfb29e56f1
Hyean jin Nam <hyeanjinnam@Hyeanui-MacBookPro.local>
2021-03-10 18:48:00 +0900

    simple_express_server_2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 app.use('/', (req, res) => {
     res.send("Hello World Node Server!");
 })
-.listen(5000, () => {
+
+app.listen(5000, () => {
     console.log('Server on Port 5000');
 })
